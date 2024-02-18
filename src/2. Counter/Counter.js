bc4d2124bb711f044fac053efd51f4fb302ccd2b
krzysztofrz <34165654+krzysztofrz@users.noreply.github.com>
2024-02-18 16:09:32 +0100

    2. Counter

---
+++ src/2. Counter/Counter.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import styles from "./Counter.module.css";
+
+const Counter = () => {
+	return null;
+};
+//
+/*
+roziwazanie
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+// const Counter = () => {
+// 	const [count, setCount] = useState(0);
+// 	return (
+// 		<div className={styles.app}>
+// 			<h1
+// 				className={
+// 					count > 0 ? styles.positive : count < 0 ? styles.negative : null
+// 				}
+// 			>
+// 				{count}
+// 			</h1>
+// 			<div className={styles.buttonWrapper}>
+// 				<button onClick={() => setCount(count - 1)}>-</button>
+// 				<button onClick={() => setCount(count + 1)}>+</button>
+// 			</div>
+// 		</div>
+// 	);
+// };
+
+export default Counter;
--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import BrowserInput from "./1. BrowserInput/BrowserInput";
+import Counter from "./2. Counter/Counter";
 
 function App() {
 	return (
 		<div className="App">
-			<BrowserInput />
+			<Counter />
 		</div>
 	);
 }
