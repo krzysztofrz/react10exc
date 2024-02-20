af77c66bd6ffc9f1a5867e68eebdae62334959d3
krzysztofrz <34165654+krzysztofrz@users.noreply.github.com>
2024-02-20 10:04:02 +0100

    3. api request update

--- src/3. APIrequest/APIrequest.js
+++ src/3. APIrequest/APIrequest.js
@@ -28,22 +28,25 @@ rozwiazanie
 
 //// axios solution
 // const ApiRequest = () => {
-// 	const [joke, setJoke] = useState({ text: "", iconUrl: "" });
-// 	// const api1 = "https://randomuser.me/api";
-// 	const api2 = "https://api.chucknorris.io/jokes/random";
+// 	const [user, setUser] = useState({ text: "", iconUrl: "" });
+// 	const api1 = "https://randomuser.me/api";
+// 	// const api2 = "https://api.chucknorris.io/jokes/random";
 // 	const getMyApi = () => {
-// 		Axios.get(api2).then((response) => {
-// 			console.log(response);
-// 			setJoke({ text: response.data.value, iconUrl: response.data.icon_url });
+// 		Axios.get(api1).then((response) => {
+// 			console.log(response.data);
+// 			setUser({
+// 				text: response.data.results[0].email,
+// 				iconUrl: response.data.results[0].picture.large,
+// 			});
 // 		});
 // 	};
 
 // 	return (
 // 		<>
 // 			<div>this is joke</div>
-// 			<p>{joke.text}</p>
-// 			<img src="{joke.iconUrl" alt="joke icon" />
-// 			<button onClick={getMyApi}>get random joke</button>
+// 			<p>{user.text}</p>
+// 			<img src={user.iconUrl} alt="face" />
+// 			<button onClick={getMyApi}>get random person</button>
 // 		</>
 // 	);
 // };
@@ -69,6 +72,43 @@ rozwiazanie
 // 			<p>{joke.text}</p>
 // 			<img src="{joke.iconUrl" alt="joke icon" />
 // 			<button onClick={getMyApi}>get random joke</button>
+// 		</>
+// 	);
+// };
+
+// export default ApiRequest;
+
+/////////////// dodanie kolejnych zdjec z mailem
+
+// const ApiRequest = () => {
+// 	const [users, setUsers] = useState([]);
+// 	const api1 = "https://randomuser.me/api";
+// 	// const api2 = "https://api.chucknorris.io/jokes/random";
+
+// 	const getMyApi = () => {
+// 		Axios.get(api1).then((response) => {
+// 			console.log(response.data);
+
+// 			const newUser = {
+// 				text: response.data.results[0].email,
+// 				iconUrl: response.data.results[0].picture.large,
+// 			};
+// 			setUsers((users) => [...users, newUser]);
+// 		});
+// 	};
+
+// 	return (
+// 		<>
+// 			<div>this is new user</div>
+// 			{users.map((user, index) => (
+// 				<div key={index}>
+// 					{" "}
+// 					{/* Uwaga: lepiej użyć bardziej unikalnego klucza niż index */}
+// 					<p>{user.text}</p>
+// 					<img src={user.iconUrl} alt="face" />
+// 				</div>
+// 			))}
+// 			<button onClick={getMyApi}>get random person</button>
 // 		</>
 // 	);
 // };
